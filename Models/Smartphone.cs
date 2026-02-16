6bd4b405ca4328d99e2d6666d7452f536db8086a
Higor Bispo <higorbispo.dev@gmail.com>
2026-02-16 11:52:15 -0300

    Desafio-POO-Trilha-Net Concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,19 +1,33 @@
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo{get;set;}
+        public string IMEI {get;set;}
+        public int Memoria{get;set;}
 
         public Smartphone(string numero)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+             Modelo = Modelo;
+                IMEI = IMEI;
+                Memoria = Memoria;
+
+                //na hora de instanciar o nokia, tem que passar os parametros do construtor, e ai sim, passar para as propriedades.  
+        }
+
+        private void InstalarAplicativo()
+        {
+            throw new NotImplementedException();
         }
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine($"Ligando para o número {Numero} ... ");
         }
 
         public void ReceberLigacao()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
-
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+// Instaciando o Nokia
+Console.WriteLine("Teste Nokia:");
+Smartphone nokia = new Nokia(numero: "123456789", modelo: "Nokia 3310", imei: "123456789012345", memoria: 16);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+
+Console.WriteLine("\n"); // Apenas para separar os testes no console
+
+
+//Instanciar o Iphone
+Console.WriteLine("Teste Iphone:");
+Smartphone iphone = new Iphone(numero: "654321987", modelo: "Iphone 13", imei: "543210987654321", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
